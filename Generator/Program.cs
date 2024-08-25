1a013b7b080c44baafe6cdce43cfcfc44748195f
Juna <46632782+JunaMeinhold@users.noreply.github.com>
2024-08-25 20:58:57 +0200

    Add project files.

---
+++ Generator/Program.cs
@@ -0,0 +1,7 @@
+﻿using HexaGen;
+using HexaGen.Patching;
+
+CsCodeGeneratorConfig config = CsCodeGeneratorConfig.Load("generator.json");
+CsCodeGenerator generator = new(config);
+generator.PatchEngine.RegisterPrePatch(new NamingPatch(["B2"], NamingPatchOptions.None));
+generator.Generate([.. Directory.GetFiles("include")], "../../../../Hexa.NET.Box2D/Generated");
